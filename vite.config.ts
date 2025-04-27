f0a8c03ee87da7499cf1bb1722aa0d0f67b26a71
Morr1s <30533815+Morr1s@users.noreply.github.com>
2025-04-28 00:26:14 +0200

    rere

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/postcss' // Correct import for PostCSS plugin
-import autoprefixer from 'autoprefixer' // Import autoprefixer
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/sh-calc/',
   plugins: [react()],
-  css: { // Add explicit PostCSS configuration
-    postcss: {
-      plugins: [
-        tailwindcss, // Use the correct plugin
-        autoprefixer
-      ],
-    },
-  },
 })
